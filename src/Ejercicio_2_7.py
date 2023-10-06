1805e70ef16a892f97944be8a025294b4cbe5829
Rubén <rubencelislobato@gmauil.com>
2023-10-06 14:23:52 +0200

    Cambios realizados en el Ejercicio_2_7.py y en el test_ej7.py

--- src/Ejercicio_2_7.py
+++ src/Ejercicio_2_7.py
@@ -1,6 +1,8 @@
 # Escribe un programa que solicite tres números al usuario y calcule e imprima por pantalla su suma.
 
 def calculo_num(numero1, numero2, numero3):
+    if numero1 < 0 or numero2 < 0 or numero3 < 0:
+        return "Los números introducidos no deben de ser negativos"
     suma = numero1 + numero2 + numero3
     return "La suma de sus números es " + str(suma)
 
--- test/test_ej7.py
+++ test/test_ej7.py
@@ -2,5 +2,5 @@ from src.Ejercicio_2_7 import calculo_num
 
 def test_calculo_num():
     assert calculo_num(3, 5, 2) == "La suma de sus números es 10"
-    assert calculo_num(7, 2, 3) == "La suma de sus números es 12"
-    assert calculo_num(8, 9, 1) == "La suma de sus números es 18"
+    assert calculo_num(0, 0, 0) == "La suma de sus números es 0"
+    assert calculo_num(-8, 9, 1) == "Los números introducidos no deben de ser negativos"
