1eece12e3309011cdea945e140463db6f23d7c97
Rubén <rubencelislobato@gmauil.com>
2023-10-06 15:09:56 +0200

    Cambios realizados en Ejercicio_2_13.py y en test_ej13.py

--- src/Ejercicio_2_13.py
+++ src/Ejercicio_2_13.py
@@ -3,13 +3,19 @@
 # por el usuario, y c y r son el cociente y el resto de la división entera respectivamente.
 
 def division_prog(numero1, numero2):
-    cociente = numero1 // numero2
-    resto = numero1 % numero2
-    mensaje = f"La división de {numero1} entre {numero2} da un cociente {cociente} y un resto {resto}."
-    return mensaje
+    if numero2 == 0:
+        return "La división por cero no está permitida."
+    elif numero1 < 0 or numero2 < 0:
+        return "La división no se puede realizar correctamente debido a números negativos."
+    else:
+        cociente = numero1 // numero2
+        resto = numero1 % numero2
+        mensaje = f"La división de {numero1} entre {numero2} da un cociente {cociente} y un resto {resto}."
+        return mensaje
 
 if __name__ == "__main__":
     numero1 = int(input("Escribe un número entero: "))
     numero2 = int(input("Escribe otro número entero: "))
     print(division_prog(numero1, numero2))
+
 
--- test/test_ej13.py
+++ test/test_ej13.py
@@ -2,5 +2,6 @@ from src.Ejercicio_2_13 import division_prog
 
 def test_division_prog():
     assert division_prog(8, 4) == "La división de 8 entre 4 da un cociente 2 y un resto 0."
-    assert division_prog(4, 2) == "La división de 4 entre 2 da un cociente 2 y un resto 0."
-    assert division_prog(6, 3) == "La división de 6 entre 3 da un cociente 2 y un resto 0."
+    assert division_prog(0, 0) == "La división por cero no está permitida."
+    assert division_prog(-6, 3) == "La división no se puede realizar correctamente debido a números negativos."
+    assert division_prog(6, -3) == "La división no se puede realizar correctamente debido a números negativos."
