355eeb742db50fd3e8d46d98f14e7dba1bd4bb9b
Rubén <rubencelislobato@gmauil.com>
2023-10-06 13:30:57 +0200

    Modificacion en test_ej1.py

--- test/test_ej1.py
+++ test/test_ej1.py
@@ -3,5 +3,9 @@ from src.Ejercicio_2_1 import obtener_saludo
 def test_obtener_saludo():
     nombre = "Juan"
     nombre2 = "Paco"
+    nombre3 = ""
+    nombre4 = "\n"
     assert obtener_saludo(nombre) == "Hola, Juan"
     assert obtener_saludo(nombre2) == "Hola, Paco"
+    assert obtener_saludo(nombre3) == "Hola, "
+    assert obtener_saludo(nombre4) == "Hola, \n"
