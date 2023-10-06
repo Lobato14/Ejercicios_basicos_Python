31f5f8e3c58225ebaa79748771bb35c3d315d7af
Rubén <rubencelislobato@gmauil.com>
2023-10-06 14:00:06 +0200

    Cambios realizados en el Ejercicio_2_2.py y en test_ej2.py

--- src/Ejercicio_2_2.py
+++ src/Ejercicio_2_2.py
@@ -2,10 +2,17 @@
 # por hora y calcule el importe total del servicio.
 
 def calcular_importe(horas, precio):
-    importe = horas * precio
-    return importe
-
-if __name__=="__main__":
+    if horas < 0 or precio < 0:
+        return "Las horas y el precio deben ser números positivos"
+    else:
+        importe = horas * precio
+        return importe
+    
+if __name__ == "__main__":
     horas = int(input("Escribe las horas trabajadas: "))
     precio = int(input("Escribe el precio trabajado: "))
-    print(calcular_importe(horas, precio))
+    if horas < 0 or precio < 0:
+        print("Las horas y el precio deben ser números positivos")
+    else:
+        resultado = calcular_importe(horas, precio)
+        print("El importe total del servicio es de", resultado)
--- test/test_ej2.py
+++ test/test_ej2.py
@@ -1,6 +1,7 @@
 from src.Ejercicio_2_2 import calcular_importe
 
 def test_calcular_importe():
-    assert calcular_importe(5, 10) == 50
-    assert calcular_importe(0, 20) == 0
-    assert calcular_importe(10, 10) == 100
+    assert calcular_importe(0, 100) == 0
+    assert calcular_importe(5, 20) == 100
+    assert calcular_importe(-1, -15) == "Las horas y el precio deben ser números positivos"
+    assert calcular_importe(15, -14) == "Las horas y el precio deben ser números positivos"
