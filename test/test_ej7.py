5c08ad2069541fdea6d2a9d0ce7909bbb3bfe5c1
Rubén <rubencelislobato@gmauil.com>
2023-10-06 14:25:16 +0200

    Cambios en Ejercicio_2_7.py y en el test_ej7.py

--- test/test_ej7.py
+++ test/test_ej7.py
@@ -3,4 +3,4 @@ from src.Ejercicio_2_7 import calculo_num
 def test_calculo_num():
     assert calculo_num(3, 5, 2) == "La suma de sus números es 10"
     assert calculo_num(0, 0, 0) == "La suma de sus números es 0"
-    assert calculo_num(-8, 9, 1) == "Los números introducidos no deben de ser negativos"
+    assert calculo_num(-8, 7, 1) == "Los números introducidos no deben de ser negativos"
