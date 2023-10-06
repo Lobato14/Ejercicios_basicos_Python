ac2f9f384658e3ad1e1c0fe9835c7847c8b01c67
Rubén <rubencelislobato@gmauil.com>
2023-10-06 14:47:04 +0200

    Cambios realizados Ejercicio_2_9.py y en test_ej9.py

--- src/Ejercicio_2_9.py
+++ src/Ejercicio_2_9.py
@@ -1,8 +1,7 @@
 # ¿Es posible escribir el programa del ejercicio 1.7 sin usar variables? Inténtalo.
 
 def suma_numeros():
-    suma = int(input("Ingresa el primer número: ")) + int(input("Ingresa el segundo número: ")) + int(input("Ingresa el tercer número: "))
-    return "La suma de sus números es de " + str(suma)
+    return "La suma de sus números es de " + str(int(input("Ingresa el primer número: ")) + int(input("Ingresa el segundo número: ")) + int(input("Ingresa el tercer número: ")))
 
 if __name__ == "__main__":
     print("Ingresa tres números: ")
--- test/test_ej9.py
+++ test/test_ej9.py
@@ -2,3 +2,4 @@ from src.Ejercicio_2_9 import suma_numeros
 
 def test_suma_numeros():
     assert suma_numeros() == "La suma de sus números es de 6"
+    assert suma_numeros() == "La suma de sus números es de 0"
