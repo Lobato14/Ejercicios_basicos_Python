c2d3abebe84bbb5597f91b2e26b8b897baa2eda3
Rubén <rubencelislobato@gmauil.com>
2023-10-06 16:51:40 +0200

    Corrección Ejercicio_2_26.py y del test_ej26.py

--- src/Ejercicio_2_26.py
+++ src/Ejercicio_2_26.py
@@ -2,12 +2,14 @@
 # y muestre por pantalla cada uno de los productos en una línea distinta.
 
 def mostrar_lista_compra(lista_compra):
-    productos = lista_compra.split(",")
-    resultado = ""
-    for producto in productos:
-        resultado += producto.strip() + "\n"
-    return resultado
-
+    if not lista_compra or "," not in lista_compra:
+        return "Error: La lista de compra está vacía o no contiene productos separados por comas."
+    else:
+        productos = lista_compra.split(",")
+        resultado = ""
+        for producto in productos:
+            resultado += producto.strip() + "\n"
+        return resultado
 
 if __name__ == "__main__":
     lista_compra = input("Escriba los productos de una cesta de la compra, separados por comas: ")
--- test/test_ej26.py
+++ test/test_ej26.py
@@ -1,5 +1,7 @@
 from src.Ejercicio_2_26 import mostrar_lista_compra
 
 def test_mostrar_lista_compra():
-    assert mostrar_lista_compra("huevos, platanos, manzanas") == "huevos\nplatanos\nmanzanas\n"
-    assert mostrar_lista_compra("sardinas, mangos , zumo, coca-cola") == "sardinas\nmangos\nzumo\ncoca-cola\n"
+    assert mostrar_lista_compra("manzanas, peras, plátanos") == "manzanas\nperas\nplátanos\n"
+    assert mostrar_lista_compra("leche") == "Error: La lista de compra está vacía o no contiene productos separados por comas."
+    assert mostrar_lista_compra("") == "Error: La lista de compra está vacía o no contiene productos separados por comas."
+    assert mostrar_lista_compra("manzanas;peras;plátanos") == "Error: La lista de compra está vacía o no contiene productos separados por comas."
