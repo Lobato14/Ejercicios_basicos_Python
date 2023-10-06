98ea75a901ca8a69ea3a62b5f77dceab97c9e63e
Rubén <rubencelislobato@gmauil.com>
2023-10-06 15:52:12 +0200

    Cambios realizados en el Ejercicio_2_16.py y en el test_ej16.py

--- src/Ejercicio_2_16.py
+++ src/Ejercicio_2_16.py
@@ -4,16 +4,17 @@
 # una constante), el descuento que se le hace por no ser fresca y el coste final total de todas las barras no frescas.
 
 def calcular_costo_pan(no_pan_Dia):
+    if no_pan_Dia <=0 :
+        return "El numero de barras de pan vendidas no puede ser menor o igual a 0."
     pvpPan = 3.49
     descuento = 0.6
     calcDesc = round(pvpPan * descuento, 2)
     costo_total = (pvpPan - calcDesc) * no_pan_Dia
     return pvpPan, calcDesc, round(costo_total, 2)
 
-# Ejemplo de uso de la función
-no_pan_Dia = 5 
-precio_pan, descuento, costo_total = calcular_costo_pan(no_pan_Dia)
-
-print("Precio de una barra de pan:", precio_pan, "€")
-print("Descuento por ser fresca:", descuento, "€")
-print("Coste total de las barras no frescas:", costo_total, "€")
+if __name__ == "__main__":
+    no_pan_Dia = int(input("Número de barras vendidas que no son del día: "))
+    precio_pan, descuento, costo_total = calcular_costo_pan()
+    print("Precio de una barra de pan:", precio_pan, "€")
+    print("Descuento por ser fresca:", descuento, "€")
+    print("Coste total de las barras no frescas:", costo_total, "€")
--- test/test_ej16.py
+++ test/test_ej16.py
@@ -2,3 +2,5 @@ from src.Ejercicio_2_16 import calcular_costo_pan
 
 def test_calculo_costo_pan():
     assert calcular_costo_pan(5) == (3.49, 2.09, 7.0)
+    assert calcular_costo_pan(0) == "El numero de barras de pan vendidas no puede ser menor o igual a 0."
+    assert calcular_costo_pan(-5) == "El numero de barras de pan vendidas no puede ser menor o igual a 0."
