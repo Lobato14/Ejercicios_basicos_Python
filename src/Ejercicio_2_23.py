a9aa37c117bf71e8e34803311e8e4af9d11d9b9d
Rubén <rubencelislobato@gmauil.com>
2023-10-06 16:16:19 +0200

    Cambios realizados en Ejercicio_2_23.py y en test_ej23.py

--- src/Ejercicio_2_23.py
+++ src/Ejercicio_2_23.py
@@ -2,10 +2,14 @@
 # electrónico con el mismo nombre (la parte delante de la arroba @) pero con dominio ceu.es.
 
 def modificarEmail(email):
-    # Dividimos el correo en dos partes: nombre y dominio con split
-    nombre, dominio = email.split("@")
-    emailMod = nombre + "@ceu.es"
-    return emailMod
+    # Verificar si la cadena contiene al menos una letra y una arroba (@)
+    if any(c.isalpha() for c in email) and "@" in email:
+        # Dividimos el correo en dos partes: nombre y dominio con split
+        nombre, dominio = email.split("@")
+        emailMod = nombre + "@ceu.es"
+        return emailMod
+    else:
+        return "Correo electrónico inválido. Debe contener al menos una letra y una arroba (@)."
 
 if __name__ == "__main__":
     email = input("Escriba su correo electronico: ")
--- test/test_ej23.py
+++ test/test_ej23.py
@@ -1,6 +1,7 @@
 from src.Ejercicio_2_23 import modificarEmail
 
 def test_modificarEmail():
-    assert modificarEmail("rubencelislobato@gmail.com") == "rubencelislobato@ceu.es"
-    assert modificarEmail("nataliajimenez@gmail.com") == "nataliajimenez@ceu.es"
-    assert modificarEmail("esmeraldabenitez@gmail.com") == "esmeraldabenitez@ceu.es"
+    assert modificarEmail("usuario@example.com") == "usuario@ceu.es"
+    assert modificarEmail("correo_sin_arroba.com") == "Correo electrónico inválido. Debe contener al menos una letra y una arroba (@)."
+    assert modificarEmail("123@correo_con_numero.com") == "123@ceu.es"
+    assert modificarEmail("") == "Correo electrónico inválido. Debe contener al menos una letra y una arroba (@)."
